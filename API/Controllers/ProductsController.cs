1c009cf29f53d21d91434ce4a4a1f0802b31c5fc
Taufiqul Islam <tosunbd@gmail.com>
2022-12-24 19:27:03 +0600

    Initial Commit

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -25,10 +25,8 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
-            return product;
-        }
-       
+            return await _context.Products.FindAsync(id);            
+        }      
        
     }
 }
